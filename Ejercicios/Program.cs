980f4a0eeb10a31aafc4063c07077750b7a69770
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-08-16 21:10:13 -0500

    Eliminar código comentado en el archivo Program.cs

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -11,30 +11,3 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
     }
 }
-
-
-
-// See https://aka.ms/new-console-template for more information
-/*
-fizzBuzz();
-
-void fizzBuzz() 
-{
-    for (int index=1; index <= 100; index++)
-    {
-        bool divisibleByTree = index % 3 == 0;
-        bool divisibleByFive = index % 5 == 0;
-
-        if (divisibleByTree && divisibleByFive)
-        {
-            Console.WriteLine("fizzbuzz");
-        } else if (divisibleByTree) {
-            Console.WriteLine("fizz");
-        } else if (divisibleByFive) {
-            Console.WriteLine("buzz");
-        } else {
-            Console.WriteLine(index);
-        }
-    }
-}
-*/
