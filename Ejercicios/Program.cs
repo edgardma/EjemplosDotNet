e09e202e741d33cafc215604bae8021dab77ce85
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-09-10 23:12:17 -0500

    Desarrollo del Challenge 6

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -14,9 +14,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //Challenge2.Fibonacci();
             //Challenge3.PrintFirst100PrimeNumbers();
 
-            Challenge4.Area(new Triangle(10.0, 5.0));
-            Challenge4.Area(new Rectangle(5.0, 7.0));
-            Challenge4.Area(new Square(4.0));
+            //Challenge4.Area(new Triangle(10.0, 5.0));
+            //Challenge4.Area(new Rectangle(5.0, 7.0));
+            //Challenge4.Area(new Square(4.0));
+
+            Console.WriteLine(Challenge6.Reverse("Hola mundo"));
+            Console.WriteLine(Challenge6.RecursiveReverse("Hola mundo"));
         }
     }
 }
